125d3167d22b8b9d3901e207281300dcdb9d4434
Ryan Tablada <ryan.tablada@gmail.com>
2013-10-15 18:17:07 -0500

    Remove dependancy on Config Facade and updated to follow Laravel Style guide

--- src/config/config.php
+++ src/config/config.php
@@ -3,18 +3,18 @@
 
 return array(
 
-    /*
-    |--------------------------------------------------------------------------
-    | API Secret Key
-    |--------------------------------------------------------------------------
-    |
-    | The api secret key provided by Mailchimp. If you don't know your API key
-    | you can find out how to get it here:
-    | "http://kb.mailchimp.com/article/where-can-i-find-my-api-key/"
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| API Secret Key
+	|--------------------------------------------------------------------------
+	|
+	| The api secret key provided by Mailchimp. If you don't know your API key
+	| you can find out how to get it here:
+	| "http://kb.mailchimp.com/article/where-can-i-find-my-api-key/"
+	|
+	*/
 
-    'apikey' => 'your-key-here',
+	'apikey' => 'your-key-here',
 
 
 );
