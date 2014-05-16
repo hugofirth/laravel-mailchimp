ceae101410d99d44cc367ffaec01838072c61a0e
Travis Vignon <travis@lookitsatravis.com>
2014-05-16 11:20:30 -0500

    Updated wrapper to use magic __call instead of tightly coupled methods

--- src/Hugofirth/Mailchimp/MailchimpWrapper.php
+++ src/Hugofirth/Mailchimp/MailchimpWrapper.php
@@ -22,126 +22,16 @@ class MailchimpWrapper {
 		$this->mc = $mc;
 	}
 
-	//Accessors for public member objects beyond this point
+    public function __call($method, array $args)
+    {
+        //If it's a method, call it
+        if(method_exists($this->mc, $method))
+        {
+            return call_user_func_array(array($this->mc, $method), $args);
+        }
 
-	/**
-	 * Get Mailchimp_Folders object
-	 *
-	 * @return Mailchimp_Folders
-	 */
-	public function folders()
-	{
-		return $this->mc->folders;
-	}
-
-	/**
-	 * Get Mailchimp_Templates object
-	 *
-	 * @return Mailchimp_Templates
-	 */
-	public function templates()
-	{
-		return $this->mc->templates;
-	}
-
-	/**
-	 * Get Mailchimp_Users object
-	 *
-	 * @return Mailchimp_Users
-	 */
-	public function users()
-	{
-		return $this->mc->users;
-	}
-
-	/**
-	 * Get Mailchimp_Helper object
-	 *
-	 * @return Mailchimp_Helper
-	 */
-	public function helper()
-	{
-		return $this->mc->helper;
-	}
-
-	/**
-	 * Get Mailchimp_Mobile object
-	 *
-	 * @return Mailchimp_Mobile
-	 */
-	public function mobile()
-	{
-		return $this->mc->mobile;
-	}
-
-	/**
-	 * Get Mailchimp_Ecomm object
-	 *
-	 * @return Mailchimp_Ecomm
-	 */
-	public function ecomm()
-	{
-		return $this->mc->ecomm;
-	}
-
-	/**
-	 * Get Mailchimp_Neapolitan object
-	 *
-	 * @return Mailchimp_Neapolitan
-	 */
-	public function neapolitan()
-	{
-		return $this->mc->neapolitan;
-	}
-
-	/**
-	 * Get Mailchimp_Lists object
-	 *
-	 * @return Mailchimp_Lists
-	 */
-	public function lists()
-	{
-		return $this->mc->lists;
-	}
-
-	/**
-	 * Get Mailchimp_Campaigns object
-	 *
-	 * @return Mailchimp_Campaigns
-	 */
-	public function campaigns()
-	{
-		return $this->mc->campaigns;
-	}
-
-	/**
-	 * Get Mailchimp_Vip object
-	 *
-	 * @return Mailchimp_Vip
-	 */
-	public function vip()
-	{
-		return $this->mc->vip;
-	}
-
-	/**
-	 * Get Mailchimp_Reports object
-	 *
-	 * @return Mailchimp_Reports
-	 */
-	public function reports()
-	{
-		return $this->mc->reports;
-	}
-
-	/**
-	 * Get Mailchimp_Gallery object
-	 *
-	 * @return Mailchimp_Gallery
-	 */
-	public function gallery()
-	{
-		return $this->mc->gallery;
-	}
+        //Otherwise, treat it as a property
+        return $this->mc->{$method};
+    }
 
 }
